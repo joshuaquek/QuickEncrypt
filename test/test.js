731b7fa7a7656e137d51a7b3ed365af1b23d0ff0
Lewis Cowper <lewis.cowper@googlemail.com>
2018-09-15 19:59:07 +0200

    Add test suites that should fail properly instead of just erroring out

--- test/test.js
+++ test/test.js
@@ -1,26 +1,37 @@
+const test = require('tape')
+
 const { generate, encrypt, decrypt } = require('../index')
-console.log("\n\n\n\n")
 
+const message = 'This is some super top secret text!'
 
-// --- Test 1: Keypair Generation ---
-console.log(" --- Test 1: Keypair Generation --- \n")
-let keys = generate(1024)
-console.log(keys)
-console.log("\n\n\n\n")
+test('QuickEncrypt tests', function(t) {
+  const keys = generate(1024)
 
+  t.test('Keypair Generation', function (t) {
+    t.plan(5)
+    t.ok(keys, 'Check keys are defined')
+    t.equal(keys.public.slice(0, 31), '-----BEGIN RSA PUBLIC KEY-----\n', 'Public key begins correctly')
+    t.equal(keys.public.slice(-29), '-----END RSA PUBLIC KEY-----\n', 'Public key ends correctly')
+    t.equal(keys.private.slice(0, 32), '-----BEGIN RSA PRIVATE KEY-----\n', 'Private key begins correctly')
+    t.equal(keys.private.slice(-30), '-----END RSA PRIVATE KEY-----\n', 'Private key ends correctly')
+  })
 
-// --- Test 2: Encrypt using public key ---
-console.log(" --- Test 2: Encrypt using public key --- \n")
-let publicKey = keys.public
-let encryptedText = encrypt( "This is some super top secret text!", publicKey )
-console.log(encryptedText)
-console.log("\n\n\n\n")
+  let encryptedText;
 
+  // --- Test 2: Encrypt using public key ---
+  t.test('Encryption using public key', function (t) {
+    t.plan(2)
+    t.ok(encrypt(message, keys.public), 'Encryption succeeds')
+    encryptedText = encrypt(message, keys.public);
 
-// --- Test 3: Decrypt using private key ---
-console.log(" --- Test 3: Decrypt using private key --- \n")
-let privateKey = keys.private
-let decryptedText = decrypt( encryptedText, privateKey)
-console.log(decryptedText)
-console.log("\n\n\n\n")
+    let decryptedText;
+    // --- Test 3: Decrypt using private key ---
+    t.test('Decryption using private key', function (t) {
+      t.plan(2)
+      t.ok(decrypt(encryptedText, keys.private), 'Decryption succeeds')
+      decryptedText = decrypt(encryptedText, keys.private);
+      t.equal(decryptedText, message, 'Message is correctly decrypted')
+    })
+  })
+})
 
